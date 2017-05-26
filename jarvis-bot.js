25b2dcd772ecab920639fe389193b495366e8ca8
Mike Stevens <smartguy1044@gmail.com>
2017-05-26 15:26:19 -0400

    Add youtube search

--- jarvis-bot.js
+++ jarvis-bot.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const request = require('request');
-const client  = new Discord.Client();
+const youtube_node = require('youtube-node');
 const config  = require('./config.json');
+const client  = new Discord.Client();
 
 client.login(config.token);
 
@@ -56,51 +57,91 @@ client.on('message', (message) => {
     });
   }
 
-if (message.content.startsWith(prefix + "img"))
-{
-  // Split message to search image
-  let splitWord = message.toString().split(" ");
-  let searchWrd = "";
-  let googKey   = config.googleKey;
-  let cxKey     = config.cx;
-
-  // Loop through incase of multiple word search
-  for( var i = 1; i < splitWord.length; i++)
+  if (message.content.startsWith(prefix + "img"))
   {
-    if(i > 1)
+    // Split message to search image
+    let splitWord = message.toString().split(" ");
+    let searchWrd = "";
+    let googKey   = config.googleKey;
+    let cxKey     = config.cx;
+
+    // Loop through incase of multiple word search
+    for( var i = 1; i < splitWord.length; i++)
     {
-      searchWrd = searchWrd + " ";
+      if(i > 1)
+      {
+        searchWrd = searchWrd + " ";
+      }
+
+      searchWrd = searchWrd + splitWord[i];
     }
 
-    searchWrd = searchWrd + splitWord[i];
+    let page = 1;
+
+    request("https://www.googleapis.com/customsearch/v1?key=" + googKey + "&cx=" + cxKey + "&q=" + searchWrd + "&searchType=image&alt=json&num=10&start="+page, function(err, res, body) {
+      let data;
+
+      try {
+        data = JSON.parse(body);
+      } catch (error) {
+        console.log(error)
+        return;
+      }
+
+      if(!data){
+        console.log(data);
+        message.channel.sendMessage( "Error:\n" + JSON.stringify(data));
+        return;
+      } else if (!data.items || data.items.length == 0){
+        console.log(data);
+        message.channel.sendMessage( "No result for '" + args + "'");
+        return;
+      }
+      // Get random number
+      let ranNum = Math.floor(Math.random() * data.items.length);
+      let randResult = data.items[ranNum];
+      message.channel.sendMessage( randResult.title + '\n' + randResult.link);
+    });
   }
 
-  let page = 1;
+  if (message.content.startsWith(prefix + "youtube"))
+  {
+    // Split message to search image
+    let splitWord = message.toString().split(" ");
+    let searchWrd = "";
+    this.youtube = new youtube_node();
+    this.youtube.setKey(config.googleKey);
+    this.youtube.addParam('type', 'video');
 
-  request("https://www.googleapis.com/customsearch/v1?key=" + googKey + "&cx=" + cxKey + "&q=" + searchWrd + "&searchType=image&alt=json&num=10&start="+page, function(err, res, body) {
-    let data;
+    // Loop through incase of multiple word search
+    for( var i = 1; i < splitWord.length; i++)
+    {
+      if(i > 1)
+      {
+        searchWrd = searchWrd + " ";
+      }
 
-    try {
-      data = JSON.parse(body);
-    } catch (error) {
-      console.log(error)
-      return;
+      searchWrd = searchWrd + splitWord[i];
     }
 
-    if(!data){
-      console.log(data);
-      message.channel.sendMessage( "Error:\n" + JSON.stringify(data));
-      return;
-    } else if (!data.items || data.items.length == 0){
-      console.log(data);
-      message.channel.sendMessage( "No result for '" + args + "'");
-      return;
-    }
-    // Get random number
-    let ranNum = Math.floor(Math.random() * data.items.length);
-    let randResult = data.items[ranNum];
-    message.channel.sendMessage( randResult.title + '\n' + randResult.link);
-  });
-}
+    this.youtube.search(searchWrd, 1, function(error, result) {
+      if (error)
+      {
+        console.log(error);
+        message.channel.sendMessage("¯\\_(ツ)_/¯");
+      }
+      else {
+        if (!result || !result.items || result.items.length < 1)
+        {
+          message.channel.sendMessage("¯\\_(ツ)_/¯");
+        } else {
+          // Get random number
+          let ranNum = Math.floor(Math.random() * result.items.length);
+          let randResult = result.items[ranNum].id.videoId;
+          message.channel.sendMessage("http://www.youtube.com/watch?v=" + randResult);
+        }
+      }
+    });
+  }
 
 });
