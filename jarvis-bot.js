c6c1f983135fcec8de8dab9f7528394586d9f982
Mike Stevens <smartguy1044@gmail.com>
2017-05-25 20:49:05 -0400

    Add giphy lookup ability

--- jarvis-bot.js
+++ jarvis-bot.js
@@ -1,8 +1,15 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const config = require('./config.json');
+const request = require('request');
+
+const client  = new Discord.Client();
+const config  = require('./config.json');
 
 client.login(config.token);
+
+// Show message when jarvis comes online
+client.on('ready', () => {
+  console.log('I am ready!');
+});
 
 client.on('message', (message) => {
 
@@ -11,11 +18,43 @@ client.on('message', (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  if (message.content.startsWith(prefix + 'ping')) 
+  if (message.content.startsWith(prefix + "giphy")) 
   {
-    message.channel.send('pong!');
-  } else if (message.content.startsWith(prefix + 'foo')) {
-    message.channel.send('bar!');
+    // Split message to search GIPHY
+    let splitWord = message.toString().split(" ");
+    let gifWord   = "";
+
+    // Loop through incase of multiple word search
+    for( var i = 1; i < splitWord.length; i++)
+    {
+      if(i > 1)
+      {
+        gifWord = gifWord + "+";
+      }
+
+      gifWord = gifWord + splitWord[i];
+    }
+
+    request("http://api.giphy.com/v1/gifs/search?q=" + gifWord + "&api_key=" + config.giphyKey + "&limit=100", function (error, response, body) 
+    {
+      if (!error && response.statusCode == 200) 
+      {
+        // Convert body to JSON object
+        let jsonUrl = JSON.parse(body);
+
+        // Get random number to choose GIF
+        let totGif = jsonUrl.data.length;
+
+        if(totGif > 100)
+        {
+          totGif = 100;
+        }
+
+        let ranNum = Math.floor(Math.random() * totGif);
+
+        message.channel.sendMessage(jsonUrl.data[ranNum].url);
+      }
+    });
   }
 
 });
